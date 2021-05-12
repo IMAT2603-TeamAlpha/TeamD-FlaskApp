1de6d6cfb8e28af9900ed937403e7638c3384c9c
Dominic <dom@dominic.sk>
2021-05-12 21:26:10 +0100

    Upload files
    Copied from Wedding Site - change over code to start producing 'Car Sales' task.

---
+++ web/app/main/routes.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, render_template, redirect, url_for, request
+from app.models import User
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+@main.route('/index', methods=['GET', 'POST'])
+def index(cat=None):
+    return render_template('index.html',  title='Home')
+
+@main.route('/about', methods=['GET', 'POST'])
+def about():
+	return render_template('about.html', title='About')
+
+@main.route('/services', methods=['GET', 'POST'])
+def services():
+	return render_template('services.html', title='Our Services')
+
+@main.route('/contactus', methods=['GET', 'POST'])
+def contactUs():
+	return render_template('contact.html', title='Contact Us')
+
+@main.route('/tours', methods=['GET', 'POST'])
+def tours():
+	return render_template('tours.html', title='Book A Tour')
