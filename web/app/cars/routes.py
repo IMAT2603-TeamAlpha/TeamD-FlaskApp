eefe66b76fc41282c879e6f3d7586eff02fcf904
Dominic <dom@dominic.sk>
2021-06-04 05:42:36 +0100

    Added edit and delete car options

--- web/app/cars/routes.py
+++ web/app/cars/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, flash, \
                 redirect, url_for, request, session
-from app.cars.forms import AddNewCarForm
+from app.cars.forms import AddNewCarForm, EditCarForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User, Cars
 from app.funcs import save_picture, numberFormat
@@ -73,3 +73,56 @@ def searchCars():
 		search_msg = f'{len(car)} number of car(s) found matching search criteria'
 		color = 'success'
 	return render_template('cars/search.html', title='Search Models', cars=car, search_msg=search_msg, color=color)
+
+@cars.route('/cars/edit/<id>', methods=['GET', 'POST'])
+@login_required
+def editCar(id):
+	if current_user.admin:
+		car = Cars.query.get(id)
+		form = EditCarForm(obj=car)
+		if request.method == 'GET':
+			form.populate_obj(car)
+		elif request.method == 'POST':
+			if form.update.data and form.validate_on_submit():
+				car.manufacturer = form.manufacturer.data
+				car.model = form.model.data
+				car.summary = form.summary.data
+				car.description = form.description.data
+				car.year = form.year.data
+				car.mileage = form.mileage.data
+				car.transmission = form.transmission.data
+				car.fuel = form.fuel.data
+				car.engine_size = form.engine_size.data
+				car.seats = form.seats.data
+				car.doors = form.doors.data
+				car.colour = form.colour.data
+				car.mot = form.mot.data
+				car.last_mot = form.last_mot.data
+				car.has_warranty = form.has_warranty.data
+
+				if form.photo.data:
+					car.photo = save_picture(car.photo.data)
+
+				car.price = form.price.data
+				db.session.commit()
+				flash("Car %r Updated" % car.model, 'success')
+				return redirect(url_for('cars.displayCar', id=id))
+			if form.cancel.data:
+				return redirect(url_for('cars.displayCar', id=id))
+		return render_template('cars/editCar.html', title='Edit Car', form=form)
+	else:
+		flash('This page is for site administrators only - please login with an admin account.', 'danger')
+		return redirect(url_for('main.index'))
+
+@cars.route('/cars/delete/<id>', methods=['GET', 'POST'])
+@login_required
+def deleteCar(id):
+	if current_user.admin:
+		if Cars.query.filter_by(id=id).delete():
+			db.session.commit()
+			flash('Car has been deleted', 'success')
+			return redirect(url_for('main.index'))
+		return redirect(url_for('cars.displayCar', id=id))
+	else:
+		flash('This page is for site administrators only - please login with an admin account.', 'danger')
+		return redirect(url_for('main.index'))
