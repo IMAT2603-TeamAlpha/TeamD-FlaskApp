5c1c2d45f71362aecb8d9dd2492f426c641c151b
Aliya <p2532013@dmu.ac.uk>
2021-06-10 23:54:03 +0100

    FAQ Routes.py

--- web/app/main/routes.py
+++ web/app/main/routes.py
@@ -1,21 +1,45 @@
-from flask import Blueprint, render_template, redirect, url_for, request
-from app.models import User
+from flask import Blueprint, render_template, redirect, url_for, request, session
+from app.main.forms import MainForm
+from flask_login import current_user, login_user, logout_user, login_required
+from app.models import First_name, Last_name, query
+from app.funcs import save_picture
+from app import db
+from sqlalchemy.sql import func, or_
+from flask_login import current_user, login_user, logout_user, login_required
+
 
 main = Blueprint('main', __name__)
 
-@main.route('/')
-@main.route('/index', methods=['GET', 'POST'])
-def index(cat=None):
-    return render_template('index.html',  title='Quality Used Cars in Leicester')
+@main.route('/all_faq')
+@login_required
+def all_faq():
+    return 'All faq'
 
-@main.route('/about', methods=['GET', 'POST'])
-def about():
-	return render_template('about.html', title='About')
+@main.route('/FAQ', methods=['GET', 'POST'])
+@login_required
+def faq():
 
-@main.route('/services', methods=['GET', 'POST'])
-def services():
-	return render_template('services.html', title='Our Services')
-
-@main.route('/contactus', methods=['GET', 'POST'])
-def contactUs():
-	return render_template('contact.html', title='Contact Us')
+if current_user.admin:
+	form = AddNewFAQ()
+	if form.validate_on_submit():
+		photo_file = 'default.png'
+		if form.photo.data:
+			photo_file = save_picture(form.photo.data)
+        
+        faq = FAQ (
+    first_name=form.first_name.data,
+	last_name=form.last_name.data,
+	store= form.store.data,
+	query= form.query.data,
+              
+        )
+        db.session.add(faq)
+        db.session.flush()
+        new_id = faq.id
+        db.session.commit()
+        flash('faq was added successfully', 'success')
+        return render_template('faq.html', title='Ass new faq', form=form)
+	else:
+		flash('This page is for site administrators only - please login with an admin account.', 'danger')
+		return redirect(url_for('main.index'))
+    
