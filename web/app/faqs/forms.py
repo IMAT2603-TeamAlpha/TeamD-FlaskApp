1d600b4d1ffbb28bec1b79f36536f40c24a4f4a6
Dominic <dom@dominic.sk>
2021-06-11 06:41:05 +0100

    Questions (FAQ) fully implemented
    Also added; Questions Inbox w/ Admin Controls (to be used by admins only)

--- web/app/faqs/forms.py
+++ web/app/faqs/forms.py
@@ -7,6 +7,8 @@ from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Le
 class SubmitQueryForm(FlaskForm):
 	first_name = StringField('First Name', validators=[DataRequired(), Length(min=3, max=30)])
 	last_name = StringField('Last Name', validators=[DataRequired(), Length(min=3, max=30)])
+	email = StringField('Email', validators=[DataRequired()])
+	telephone = StringField('Telephone', validators=[DataRequired()])
 	store = SelectField(u'Store/Branch Location', choices=[('Leicester'), ('Nottingham'), ('London'), ('Brighton')], validators=[DataRequired()])
-	query = TextAreaField('Your Question', validators=[DataRequired()])
+	question = TextAreaField('Your Question', validators=[DataRequired()])
 	submit = SubmitField('Submit Question')
--- web/app/faqs/routes.py
+++ web/app/faqs/routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_login import current_user, login_user, logout_user, login_required
 from app.models import Questions
 from app.faqs.forms import SubmitQueryForm
 from app import db
@@ -10,15 +11,44 @@ faqs = Blueprint('faqs', __name__)
 def addQuestion():
 	form = SubmitQueryForm()
 	if form.validate_on_submit():
-		queryForm = Questions(first_name=form.first_name.data, last_name=form.last_name.data, store=form.store.data, query=form.query.data)
-		db.session.add(queryForm)
+		questionForm = Questions(first_name=form.first_name.data, last_name=form.last_name.data, email=form.email.data, telephone=form.telephone.data, store=form.store.data, question=form.question.data)
+		db.session.add(questionForm)
 		db.session.flush()
-		new_id = queryForm.id
+		new_id = questionForm.id
 		db.session.commit()
-		flash("Thanks for asking. We will endeavour to respond to your question(s) within 2 business days.", 'success')
+		flash("Thanks for asking. We will endeavour to respond to your question(s) within 1-2 business days.", 'success')
 	return render_template('faqs/addQuestion.html', title='Submit Question', form=form)
 
 @faqs.route('/questions/inbox', methods=['GET'])
+@login_required
 def displayQuestions():
-	#questions = Questions.query.filter(Questions.query.contains('')).all()
-	return render_template('faqs/questionsInbox.html', title='Questions Inbox', questions=questions)
+	if current_user.admin:
+		questions = Questions.query.filter(Questions.question.contains('')).all()
+		return render_template('faqs/questionsInbox.html', title='Questions Inbox', questions=questions)
+	else:
+		flash('This page is for site administrators only - please login with an admin account.', 'danger')
+		return redirect(url_for('main.index'))
+
+@faqs.route('/questions/inbox/<id>', methods=['GET'])
+@login_required
+def displayQuestion(id):
+	if current_user.admin:
+		questions = Questions.query.get(id)
+		return render_template('faqs/questionsInboxMessage.html', title='Questions Inbox', questions=questions)
+	else:
+		flash('This page is for site administrators only - please login with an admin account.', 'danger')
+		return redirect(url_for('main.index'))
+
+@faqs.route('/questions/delete/<id>', methods=['GET'])
+@login_required
+def deleteQuestion(id):
+	if current_user.admin:
+		if Questions.query.filter_by(id=id).delete():
+			db.session.commit()
+			db.session.commit()
+			flash('Question has been deleted.', 'success')
+			return redirect(url_for('faqs.displayQuestions'))
+		return redirect(url_for('faqs.displayQuestions'))
+	else:
+		flash('This page is for site administrators only - please login with an admin account.', 'danger')
+		return redirect(url_for('main.index'))
